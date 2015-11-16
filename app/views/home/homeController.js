c0019d661066827f79bd8ff973c11ab9184496db
Dariusz Dybka <dybka.dariusz@gmail.com>
2015-11-16 01:24:57 +0100

    dodałem opcje filtrowania plikow aktualnie wyswietlanych na stronie

--- app/views/home/homeController.js
+++ app/views/home/homeController.js
@@ -3,8 +3,6 @@ var homeModule = angular.module('eventLocatorApp.Home', []);
 
 homeModule.controller('homeController', ['$scope', 'mockData', function($scope, mockData) {
 
-
-    console.log(mockData);
     $scope.listaPlikow = mockData;
 
 }]);
